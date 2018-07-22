20112e2b9213d860d803fb9cd4888335e3f44bd4
katiejg <jng.katie@gmail.com>
2018-07-22 23:45:17 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,6 +3,7 @@ class BankAccount
   def initialize(name)
     @name=name
     @balance=1000
+    @status="open"
   end
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
 end
